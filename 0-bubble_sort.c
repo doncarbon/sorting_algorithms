e90aa1a6ab74b932af231d012a79adc5164e7fa6
Hamza Yassine <ihamzayassine@gmail.com>
2024-02-16 20:15:19 +0100

    Betty fixes, Task 0 fix

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,20 +9,20 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j;
-    int temp = 0;
+	size_t i, j;
+	int temp = 0;
 
-    for (i = 1; i <= size - 1; i++)
-    {
-        for (j = 0; j <= size - 2; j++)
-        {
-            if (array[j + 1] < array[j])
-            {
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-                print_array(array, size);
-            }
-        }
-    }
+	for (i = 1; i <= size - 1; i++)
+	{
+		for (j = 0; j <= size - 2; j++)
+		{
+			if (array[j + 1] < array[j])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
 }
