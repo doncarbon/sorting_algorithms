68d534972d504df0b77ad184e94d5ac80c2f5b8e
Hamza Yassine <ihamzayassine@gmail.com>
2024-02-17 20:34:33 +0100

    Task fixes, task 2 done

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,6 +12,9 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 	int temp = 0;
 
+	if (array == NULL || size < 2)
+		return;
+
 	for (i = 1; i <= size - 1; i++)
 	{
 		for (j = 0; j <= size - 2; j++)
--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -5,7 +5,7 @@
  * using the Selection sort algorithm.
  * @array: array to sort.
  * @size: size of the array to sort.
- * 
+ *
  * Return: Nothing (void)
 */
 void selection_sort(int *array, size_t size)
@@ -13,17 +13,25 @@ void selection_sort(int *array, size_t size)
 	size_t i, j;
 	int temp = 0;
 
+	if (array == NULL || size < 2)
+		return;
+
 	for (i = 0; i < size - 1; i++)
 	{
-		temp = array[i];
-		for (j = 0; j < size - 2; j++)
+		size_t min_index = i;
+
+		for (j = i + 1; j < size; j++)
 		{
-			if (temp > array[i + 1])
-			{
-				array[i] = array[i + 1];
-				array[i + 1] = temp;
-				print_array(array, size);
-			}
+			if (array[j] < array[min_index])
+				min_index = j;
+		}
+
+		if (min_index != i)
+		{
+			temp = array[i];
+			array[i] = array[min_index];
+			array[min_index] = temp;
+			print_array(array, size);
 		}
 	}
 }
