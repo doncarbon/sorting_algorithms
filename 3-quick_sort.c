7e42fd68dea3ea267433230367900ecbde2f1277
Hamza Yassine <ihamzayassine@gmail.com>
2024-02-17 23:02:46 +0100

    Task 3 draft

---
+++ 3-quick_sort.c
@@ -0,0 +1,15 @@
+#include "sort.h"
+
+/**
+ * quick_sort - sorts an array of integers in ascending order
+ * using the Quick sort algorithm.
+ *
+ * @array: the array to sort.
+ * @size: size of the array to sort.
+ * 
+ * Return: Nothing (void).
+*/
+void quick_sort(int *array, size_t size)
+{
+	
+}
