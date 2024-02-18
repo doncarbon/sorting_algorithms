ebd7b5314250110b5f7e0e1e3b0853f350271056
Hamza Yassine <ihamzayassine@gmail.com>
2024-02-18 21:50:00 +0100

    3-O done, Task 3 update

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,12 +1,25 @@
 #include "sort.h"
 
 /**
+ * partition - dividing the array to sort to 2 parts.
+ * @array: the array needed to be sorted.
+ * @low: the first element of the array.
+ * @high: the last element of the array.
+ * 
+ * Return: the pivot to iterate between elements of the array.
+*/
+int partition(int *array, size_t low, size_t high)
+{
+	
+}
+
+/**
  * quick_sort - sorts an array of integers in ascending order
  * using the Quick sort algorithm.
  *
  * @array: the array to sort.
  * @size: size of the array to sort.
- * 
+ *
  * Return: Nothing (void).
 */
 void quick_sort(int *array, size_t size)
