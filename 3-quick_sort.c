292d45f162e76c9676534578b29676af794d31a9
Hamza Yassine <ihamzayassine@gmail.com>
2024-02-18 22:15:06 +0100

    Task 3 done

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -8,31 +8,57 @@
  * 
  * Return: the pivot to iterate between elements of the array.
 */
-size_t partition(int *array, size_t low, size_t high)
+size_t partition(int *array, size_t low, size_t high, size_t size)
 {
-	size_t i = 0;
-	int pivot = array[low];
-	size_t left = low;
+	int pivot = array[high];
+	size_t i = low - 1;
+	size_t j;
 	int temp;
 
-	i = low + 1;
-	while (i <= high)
+	for (j = low; j <= high - 1; j++)
 	{
-		if (array[i] < pivot)
+		if (array[j] < pivot)
 		{
+			i++;
 			temp = array[i];
-			array[i] = array[left];
-			array[left] = temp;
-			left++;
+			array[i] = array[j];
+			array[j] = temp;
+			if (i != j)
+				print_array(array, size);
 		}
-		i++;
 	}
-	temp = pivot;
-	pivot = array[left];
-	array[left] = pivot;
 
-	return (left);
-	
+	temp = array[i + 1];
+	array[i + 1] = array[high];
+	array[high] = temp;
+	if ((i + 1) != high)
+		print_array(array, size);
+
+	return (i + 1);
+}
+
+/**
+ * quick_sort_recursive - helper function for recursive quick sort.
+ *
+ * @array: the array to sort.
+ * @low: the low index.
+ * @high: the high index.
+ * @size: size of the array to print.
+ *
+ * Return: Nothing (void).
+ */
+void quick_sort_recursion(int *array, size_t low, size_t high, size_t size)
+{
+	size_t pivot;
+
+	if (low < high)
+	{
+		pivot = partition(array, low, high, size);
+		if (pivot != 0)
+			quick_sort_recursion(array, low, pivot - 1, size);
+		
+		quick_sort_recursion(array, pivot + 1, high, size);
+	}
 }
 
 /**
@@ -46,13 +72,9 @@ size_t partition(int *array, size_t low, size_t high)
 */
 void quick_sort(int *array, size_t size)
 {
-	size_t low = (size + 1) - size, high = size - 1;
-	size_t pivot;
 
-	if (low < high)
-	{
-		pivot = partition(array, low, high);
-		quick_sort(array, pivot);
-		quick_sort(array, high);
-	}
+	if (array == NULL || size < 2)
+        return;
+
+	quick_sort_recursion(array, 0, size - 1, size);
 }
--- sort.h
+++ sort.h
@@ -27,5 +27,7 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+size_t partition(int *array, size_t low, size_t high, size_t size);
+void quick_sort_recursion(int *array, size_t low, size_t high, size_t size);
 
 #endif
